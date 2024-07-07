b117f78641875eb023ec51b2a5e4133496647ff0
Cristian Gimenez <gimenezc779@gmail.com>
2024-07-07 01:29:52 -0300

    Agrego modificaciones para configurar pythonanywhere

--- app.py
+++ app.py
@@ -122,15 +122,15 @@ class Catalogo:
 # Cuerpo del programa
 #--------------------------------------------------------------------
 # Crear una instancia de la clase Catalogo
-catalogo = Catalogo(host='localhost', user='root', password='', database='miapp')
-#catalogo = Catalogo(host='USUARIO.mysql.pythonanywhere-services.com', user='USUARIO', password='CLAVE', database='USUARIO$miapp')
+#catalogo = Catalogo(host='localhost', user='root', password='', database='miapp')
+catalogo = Catalogo(host='GimenezCristian.mysql.pythonanywhere-services.com', user='GimenezCristian', password='root2024', database='GimenezCristian$miapp')
 
 
 # Carpeta para guardar las imagenes.
-RUTA_DESTINO = './static/imagenes/'
+#RUTA_DESTINO = './static/imagenes/'
 
 #Al subir al servidor, deberá utilizarse la siguiente ruta. USUARIO debe ser reemplazado por el nombre de usuario de Pythonanywhere
-#RUTA_DESTINO = '/home/USUARIO/mysite/static/imagenes'
+RUTA_DESTINO = '/home/GimenezCristian/mysite/static/imagenes'
 
 
 #--------------------------------------------------------------------
